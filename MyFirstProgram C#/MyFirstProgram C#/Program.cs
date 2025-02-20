3cd42648db0a92ac43d5884bc8098e6837057a9f
A718718 <alihammad347257@gmail.com>
2025-02-20 02:53:26 +0100

    Add files via upload

---
+++ MyFirstProgram C#/MyFirstProgram C#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to MyFirstProgram!");
+            Console.WriteLine("This is my first C# program.");
+
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}! Nice to meet you.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
